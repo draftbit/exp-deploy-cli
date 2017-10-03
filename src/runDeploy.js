eabda14884822c4fd108e78d6c912deebc0eb456
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2017-10-03 16:14:45 -0500

    loading indicators and stuff

--- src/runDeploy.js
+++ src/runDeploy.js
@@ -2,19 +2,30 @@ const { promisify } = require('util');
 const { exec, spawn } = require('child_process');
 const ncp = require('ncp');
 const spawnAsync = require('@expo/spawn-async')
+const Ora = require('ora');
 
 const copyAsync = promisify(ncp);
 const execAsync = promisify(exec);
 
+const spinner = new Ora({
+  text: 'Deploying...',
+  spinner: 'pong'
+});
+
 async function runDeploy(options, cwd) {
+  spinner.start()
   const { env } = options;
 
-  const appJson = process.cwd() + '/app.json';
+  spinner.text = `Copying files over for ${env}`
+  const appJson = cwd + '/app.json'
   const fileToCopy = `./config/exp-${env}.json`;
 
   await copyAsync(fileToCopy, appJson);
+  spinner.text = 'Running exp publish'
   let expResponse = await spawnAsync('exp', ['publish']);
-  console.log('expResponse', expResponse)
+  spinner.text = `Resetting app.json`
+  let gitResponse = await spawnAsync('git', ['checkout', 'app.json'])
+  spinner.succeed();
 }
 
 module.exports = runDeploy;
