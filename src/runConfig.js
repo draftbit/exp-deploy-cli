47cdc51405ebb7f2155bacc32657959ce9b79900
Jason Brown <browniefed@gmail.com>
2018-01-09 16:14:01 -0800

    Add in custom config creator

--- src/runConfig.js
+++ src/runConfig.js
@@ -1,25 +1,33 @@
-'use strict';
+"use strict";
 
-const chalk = require('chalk');
-const exec = require('child_process').exec;
-const { promisify } = require('util');
-const fs = require('fs');
-const jsonFile = require('jsonfile');
-const mkdirp = require('mkdirp');
-const ncp = require('ncp');
-const Table = require('cli-table');
+const chalk = require("chalk");
+const exec = require("child_process").exec;
+const { promisify } = require("util");
+const fs = require("fs");
+const jsonFile = require("jsonfile");
+const mkdirp = require("mkdirp");
+const ncp = require("ncp");
+const Table = require("cli-table");
 
 const table = new Table({
-  head: ['Env', 'Filename', 'Version', 'Name', 'Slug']
+  head: ["Env", "Filename", "Version", "Name", "Slug"],
 });
 
 const copyAsync = promisify(ncp);
 const log = console.log;
 
-const CONFIG_FIELDS = ['name', 'privacy', 'slug', 'scheme', 'version'];
+const CONFIG_FIELDS = ["name", "privacy", "slug", "scheme", "version"];
 
-function parseAppJson() {
-  const file = jsonFile.readFileSync('app.json');
+const defaultEnvs = [{
+  name: "Production",
+  append: "",
+}, {
+  name: "Development",
+  append: "-staging"
+}];
+
+function parseAppJson(env) {
+  const file = jsonFile.readFileSync("app.json");
 
   if (!file) {
     log(chalk.red(`[exp-deploy config] Error: app.json doesn't exist`));
@@ -28,63 +36,62 @@ function parseAppJson() {
   }
 
   const { expo } = file;
-  copyAppJsonToConfig(file);
+  let envs = defaultEnvs;
+  if (env) {
+    envs = [
+      {
+        name: env,
+        append: "",
+      },
+    ];
+  }
+  copyAppJsonToConfig(file, envs);
 }
 
-async function copyAppJsonToConfig(file) {
-  const appJson = process.cwd() + '/app.json';
-  const expDev = './config/exp-development.json';
-  const expProd = './config/exp-production.json';
-
-  table.push([
-    'Production',
-    './config/exp-production.json',
-    file.expo.version,
-    file.expo.name,
-    file.expo.slug
-  ]);
-
+async function copyAppJsonToConfig(file, envs) {
   try {
-    const appJsonDev = Object.assign({}, file);
 
-    appJsonDev.expo.name = file.expo.name + '-staging';
-    appJsonDev.expo.slug = file.expo.slug + '-staging';
+    envs.map((env) => {
+      const appJson = Object.assign({}, file);
 
-    table.push([
-      'Development',
-      './config/exp-development.json',
-      appJsonDev.expo.version,
-      appJsonDev.expo.name,
-      appJsonDev.expo.slug
-    ]);
+      appJson.expo.name = file.expo.name + env.append;
+      appJson.expo.slug = file.expo.slug + env.append;
+
+      const fileName = `./config/exp-${env.name.toLowerCase()}.json`;
+
+      table.push([
+        env.name,
+        fileName,
+        appJson.expo.version,
+        appJson.expo.name,
+        appJson.expo.slug,
+      ]);
+
+      fs.writeFileSync(fileName, JSON.stringify(appJson, null, 2), "utf-8");
+    })
 
     console.log(table.toString());
 
-    fs.writeFileSync(
-      './config/exp-development.json',
-      JSON.stringify(appJsonDev, null, 2),
-      'utf-8'
-    );
-
-    await copyAsync(appJson, expProd);
-    log(chalk.green('[exp-deploy config]: All done! Check your config folder'));
+    log(chalk.green("[exp-deploy config]: All done! Check your config folder"));
     process.exit(0);
   } catch (err) {
-    log(chalk.red('Copying files failed: ', err));
+    log(chalk.red("Copying files failed: ", err));
     process.exitCode = 1;
     return;
   }
 }
 
 function runConfig(options, cwd) {
-  if (fs.existsSync(cwd + '/config')) {
-    log(chalk.green('[exp-deploy config]: config folder exists'));
+  const { env } = options;
+
+  if (fs.existsSync(cwd + "/config")) {
+    log(chalk.green("[exp-deploy config]: config folder exists"));
   } else {
     log(chalk.green(`[exp-deploy config]: creating config folder`));
-    mkdirp('./config');
+    mkdirp("./config");
   }
 
-  parseAppJson();
+  parseAppJson(env);
 }
 
 module.exports = runConfig;
